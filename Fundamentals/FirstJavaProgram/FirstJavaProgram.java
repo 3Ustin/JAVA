e8017042b7b6300f5e4f466c7c9867a3f7704ce8
Austin D Dupras <79591045+3Ustin@users.noreply.github.com>
2021-06-06 12:56:39 -0400

    changes

--- Fundamentals/FirstJavaProgram/FirstJavaProgram.java
+++ Fundamentals/FirstJavaProgram/FirstJavaProgram.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 public class FirstJavaProgram {
     public static void main(String[] args){
         System.out.println("My name is Austin Dupras \nI am 25 years old \nMy hometown is Marquette, MI");
