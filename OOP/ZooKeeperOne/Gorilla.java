a3f0a76215ccb0938fe8f86a4041fea9a8feaa2d
Austin D Dupras <79591045+3Ustin@users.noreply.github.com>
2021-06-07 16:31:41 -0400

    Zoo Keeper

--- OOP/ZooKeeperOne/Gorilla.java
+++ OOP/ZooKeeperOne/Gorilla.java
@@ -1,13 +1,13 @@
 public class Gorilla extends Mammal{
-    public throwSomething(){
+    public void throwSomething(){
         this.energy -= 5;
         System.out.println("Gorilla has thrown something, energy: " + this.energy);
     }
-    public eatBananas(){
+    public void eatBananas(){
         this.energy -= 10;
         System.out.println("Gorilla has eaten a banana, energy: " + this.energy);
     }
-    public climb(){
+    public void climb(){
         this.energy -= 10;
         System.out.println("Gorilla has climbed something, energy: " + this.energy);
     }
---
+++ OOP/ZooKeeperOne/Zoo.java
@@ -0,0 +1,9 @@
+public class Zoo {
+    public static void main(String[] args){
+        Gorilla george = new Gorilla();
+        george.climb();
+        george.eatBananas();
+        george.throwSomething();
+        george.displayEnergy();
+    }
+}
